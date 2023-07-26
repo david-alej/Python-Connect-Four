fc282c03ad24f9ff5794bc8ab23126d44177fab7
David <david12777@outlook.com>
2023-07-26 09:47:04 -0600

    start of connect four class

---
+++ C4.py
@@ -0,0 +1,11 @@
+class C4:
+  def __init__(self, player1, player2):
+    self.player1 = player1
+    self.player2 = player2
+    
+    
+  def initial_screen(self):
+    print('-'*30)
+    print('Welcome to Connect Four.')
+    print('-'*30)
+    print('Connect four is two player game where ')
