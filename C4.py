f68f46152dd92ca56899eef6ed72117eca16b4a9
David <david12777@outlook.com>
2023-07-27 09:34:15 -0600

    finshed the program and tested it

--- C4.py
+++ C4.py
@@ -1,21 +1,27 @@
 class C4:
-  def __init__(self, player1, player2):
+  def __init__(self, player1, player2, player1_symbol, player2_symbol):
     self.player1 = player1
     self.player2 = player2
+    self.symbol1 = player1_symbol
+    self.symbol2 = player2_symbol
     self.grid = [[0 for i in range(7)] for i in range(6)]
+    self.turn = 0
+    self.game_finished = 0
     self.initial_screen()
-    
+    self.turn += 1
     
   def initial_screen(self):
     print('-'*30)
     print('Welcome to Connect Four.')
     print('-'*30)
     print('Connect four is two player game where each player chooses a token and then take turns entering thier respective token into a grid where the piece falls to the lowest available space within the column chosen. The goal of the game is to form a horizontal, vertical, or diagonal line of four of one\'s own token.')
-    print('Let us start. The following is the empty gird.')
+    print('Let us start. The following is the empty gird. Where each player will pick a column number, shown on the top of the grid.')
     self.print_grid()
     
   def print_grid(self):
     grid = self.grid
+    print(f'\n The grid for turn {self.turn} is shown below.\n')
+    print(' 1 2 3 4 5 6 7 ')
     print('+-'*7 + '+')
     print('|{}|{}|{}|{}|{}|{}|{}|'.format(grid[0][0], grid[0][1], grid[0][2], grid[0][3], grid[0][4], grid[0][5], grid[0][6], ))
     print('+-'*7 + '+')
@@ -28,4 +34,117 @@ class C4:
     print('|{}|{}|{}|{}|{}|{}|{}|'.format(grid[4][0], grid[4][1], grid[4][2], grid[4][3], grid[4][4], grid[4][5], grid[4][6], ))
     print('+-'*7 + '+')
     print('|{}|{}|{}|{}|{}|{}|{}|'.format(grid[5][0], grid[5][1], grid[5][2], grid[5][3], grid[5][4], grid[5][5], grid[5][6], ))
-    print('+-'*7 + '+')
+    print('+-'*7 + '+\n')
+    
+  def one_turn(self, player_move):
+    column = int(player_move) - 1
+    symbol = [self.symbol1, self.symbol2]
+    if self.game_finished == 0:
+      for row in [5 - i for i in range(6)]:
+        if self.grid[row][column] == 0:
+          self.grid[row][column] = symbol[1 - self.turn % 2]
+          self.print_grid()
+          
+          self.game_status()
+          if self.game_finished == 1:
+            player = [self.player1, self.player2]
+            print(f'Congragulations \'{player[1 - self.turn % 2]}\', has won the game!!!!!!! Check the previous grid to see where \'{player[1 - self.turn % 2]}\' got the connect four.')
+            
+          self.turn += 1
+          return
+      
+      return None  
+    else:
+      print('The game is finsihed.')
+    
+  def game_status(self):
+    if self.turn < 6:
+      return 
+    
+    grid = self.grid
+    row_count = 1
+    for j in range(6):
+      for i in range(1,7):
+        if grid[j][i] == grid[j][i - 1] and grid[j][i] != 0:
+          row_count += 1
+          if row_count == 4:
+            self.game_finished = 1
+            return 1
+            
+        else:
+          row_count = 1
+            
+    column_count = 1
+    for j in range(7):
+      for i in range(1,6):
+        if grid[i][j] == grid[i - 1][j] and grid[i][j]:
+          column_count += 1
+          if column_count == 4:
+            self.game_finished = 1
+            return 1
+            
+        else:
+          column_count = 1
+    
+    for i in range(3, 6):
+      self.check_forward_slash(i + 1, i, 0)
+      if i == 5:
+        for j in range(1, 4):
+          self.check_forward_slash(i + 2 - j, i, j)
+    
+    if self.game_finished == 1:
+      return 1
+          
+    for i in range(3, 6):
+      self.check_backward_slash(i + 1, i, 6)
+      if i == 5:
+        for j in range(1, 4):
+          self.check_backward_slash(i +2 - j, i, 6 - j)
+    
+    if self.game_finished == 1:
+      return 1
+      
+    return 0
+    
+  # Helper funciton
+  def check_forward_slash(self, line_length, start_row, start_column):
+    foward_slash_count = 1
+    for i in range(1, line_length):
+      if self.grid[start_row - i][i + start_column] == self.grid[start_row - i + 1][i - 1 + start_column] and self.grid[start_row - i][i + start_column] != 0:
+        foward_slash_count += 1
+        if foward_slash_count == 4:
+          self.game_finished = 1
+          return
+        
+      else:
+        foward_slash_count = 1
+          
+  # Helper funciton
+  def check_backward_slash(self, line_length, start_row, start_column):
+    foward_slash_count = 1
+    for i in range(1, line_length):
+      if self.grid[start_row - i][start_column - i] == self.grid[start_row - i + 1][start_column - i + 1] and self.grid[start_row - i][start_column - i] != 0:
+        foward_slash_count += 1
+        if foward_slash_count == 4:
+          self.game_finished = 1
+          return
+        
+      else:
+        foward_slash_count = 1
+        
+  def reset(self):
+    self.turn = 0
+    self.game_finished = 0
+    self.grid = [[0 for i in range(7)] for i in range(6)]
+    print('\n\n')
+    self.print_grid()
+    print(f'\nNow {self.player2} will go first.\n')
+    
+  
+          
+      
+     
+
+    
+  
+        
--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from C4 import C4
 
-player1 = 'me'
-player2 = 'myself'
-game = C4(player1, player2)
+def get_symbol(player):
+  yn = input(f'\nPlease choose a token to use for \'{player}\' (It can be any character except for zero. Make sure your token is just one character) :')
+  while True:
+    if yn != '0' and len(yn) == 1:
+      return yn
+    
+    yn = input(f'\n Symbol is not permissable, please choose another token for \'{player}\' (Make sure your token is not \'0\' and it is just one character like \'@\', \'#\', \'A\', or \'4\'.) : ')
+
+def get_yn():
+  while True:
+    
+    yn = input('\n Enter a y for yes or a n for no(in lowercase please):')
+    if yn == 'y':
+      return True
+    
+    elif yn == 'n':
+      return False
+
+def get_move(player):
+  move = input(f'\n \'{player}\', enter a number from 1 to 7:')
+  while True:
+    
+    if move  in [f'{i}' for i in range(1, 8)]:
+      return move
+    
+    move = input(f' Input is invalid, \'{player}\' make sure your input is a number from 1 to 7: ')
+
+player1 = input('\nPlease choos a name for player 1: ')
+player2 = input('\nPlease choos a name for player 2: ')
+symbol1 = get_symbol(player1)
+symbol2 = get_symbol(player2)
+game = C4(player1, player2, symbol1, symbol2)
+player = [player1, player2]
+while game.game_finished == 0:
+  player_move = get_move(player[1 - game.turn % 2])
+  game.one_turn(player_move)
+  if game.game_finished == 1:
+    print('Would you like to continue the game?')
+    yn = get_yn()
+    if yn == True:
+      game.reset()
+      
+    else:
+      print('\nThank you for playing!!!')
